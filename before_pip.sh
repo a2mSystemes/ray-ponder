7050c643625631078c02530bf70bfec584c67d54
Ange-Marie MAURIN <a-m.maurin@a2msystemes.fr>
2025-03-06 04:07:18 +0100

    add wifi AP setup command in before_pip.sh

--- before_pip.sh
+++ before_pip.sh
@@ -1,3 +1,6 @@
 apt update -y
 apt install -y python-dev-is-python3 portaudio19-dev
 apt autoremove -y
+# wifi AP setup
+# use raspi-config to set wifi region
+#sudo nmcli device wifi hotspot ssid <ssid> password <password>
